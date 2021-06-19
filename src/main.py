9740a88b50dbc505780712a1ba481a17a16efbb1
Antony Alen <antonyalen1960@gmail.com>
2021-06-19 11:36:46 +0300

    Signed-off-by: Antony Alen <antonyalen1960@gmail.com>

--- src/main.py
+++ src/main.py
@@ -7,10 +7,12 @@ import random
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import pigpio
+import requests
 
 ## global variables
 read_state = True  # Check whether to keep read state on or close it
 MAX_WORKERS = 10
+URL = "https://dpkosing.azurewebsites.net/api/Messages/sms"
 
 ## Create the GSM serial objects
 ser0 = SoftwareSerial(baudrate=9600, txPin=26, rxPin=19)
@@ -31,6 +33,19 @@ def setListen(serialsList):
         dat = serObj.readString(verbose=True, timeout=0.25); print(dat)
 
 
+def postSMS(from_number, message):
+
+    print(f"Forwarding sms from {from_number}...")
+
+    def send():
+
+        r = requests.post(URL, json={'from': str(from_number), 'body': str(message)})
+        print((from_number, r.status_code, r.reason))
+    
+    threading.Thread(target=send).start()
+
+
+
 def readSMSString(serial_obj: SoftwareSerial):
     
     print(f"Listennig for sms on port {serial_obj}")
@@ -42,7 +57,19 @@ def readSMSString(serial_obj: SoftwareSerial):
             real_data = serial_obj.readSMS(timeout=1, verbose=False)
 
             if len(real_data) > 0:
-                print(f"real_data == {real_data}")
+                # print(f"received data from sim sms listen == {real_data}")
+
+                ## perse for the message if the sms is valid
+                if '+CMT' in real_data:
+                    data_list = real_data.splitlines(keepends=False)
+
+                    from_number = data_list[1].split('\"')[1]
+                    parsed_message = ' '.join(data_list[2:])
+
+                    print(f"\nNEW MESSAGE: From: {from_number} Message: {parsed_message}")
+
+                    ## Send The SMS to SERVER
+                    postSMS(from_number, parsed_message)
 
                 # except Exception as e:
                 #     if verbose: print(f"Could not decode string: {e}")
@@ -84,8 +111,6 @@ setListen(serial_ports)
 #Start the listening thread
 with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
     executor.map(readSMSString, serial_ports)
-
-
 
 
 @app.route('/send', methods=['POST'])
